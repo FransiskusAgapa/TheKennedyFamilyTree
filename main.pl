08fc8db7a0793dcce0f54f346b1538c5a89904ed
Fransiskus Agapa <agapaf@spu.edu>
2025-01-20 00:27:14 -0800

    added title of app and Kennedy's status

--- main.pl
+++ main.pl
@@ -1,5 +1,31 @@
 
-status("Frans","Billionare")
+% define rules
 
+title("Kennedy","Family Tree").
+
+status("Kennedy", "The 35th President of the US").
+
+show_title :-
+    title(Name, Title),nl,
+    write("> "),
+    write(Name),
+    write(" - "),
+    write(Title),nl.
+
+% famous for 
 show_status :-
-    status.
+    status(Name, Title),nl,
+    write("> "),
+    write(Name),
+    write(" was known for as "),
+    write(Title),nl.
+
+% parents 
+
+% wife 
+
+% children
+
+% grandchildren
+
+% Quiz
