b2d5b838b83e7ee1b684f126d8e667242f6d95dc
Fransiskus Agapa <agapaf@spu.edu>
2025-01-22 00:51:48 -0800

    added demo vid

--- main.pl
+++ main.pl
@@ -3,8 +3,21 @@
 
 title("Kennedy","Family Tree").
 
-status("Kennedy", "The 35th President of the US").
+status("Kennedy", "The 35th President of the US who was assassinated").
 
+% parents
+father("Joseph P. Kennedy Sr.","The Father").
+mother("Rose Fitzgerald Kennedy","The Mother").
+
+% wife 
+wife("Jacqueline Bouvier Kennedy", "The Wife").
+
+% children
+st_child("Caroline Kennedy","The Daughter").
+nd_child("John F. Kennedy Jr","The Son.").
+rd_child("Patrick Bouvier Kennedy","The Son.").
+
+% Present rules
 show_title :-
     title(Name, Title),nl,
     write("> "),
@@ -20,18 +33,23 @@ show_status :-
     write(" was known for as "),
     write(Title),nl.
 
-% parents
-father("Joseph P. Kennedy Sr.","The Father").
-mother("Rose Fitzgerald Kennedy","The Mother").
+show_parents :-
+    father(FaName,FaTitle),
+    mother(MoName,MoTitle),
+    write(FaTitle),write(" is "),write(FaName),
+    nl,
+    write(MoTitle),write(" is "),write(MoName).
 
-% wife 
-wife("Jacqueline Bouvier Kennedy", "The Wife").
+show_wife :-
+    wife(Name,Title),
+    write(Title),write(" is "),write(Name),write('.').
 
-% children
-first_child("Caroline Kennedy","The Daughter").
-second_child("John F. Kennedy Jr","The Son.").
-third_child("Patrick Bouvier Kennedy","The Son.").
+show_children :-
+    st_child(StName,StTitle),
+    nd_child(NdName,NdTitle),
+    rd_child(RdName,RdTitle),
+    write("The 1st child is "),write(StTitle),write(", "),write(StName),write('.'),nl,
+    write("The 2nd child is "),write(NdTitle),write(", "),write(NdName),write('.'),nl,
+    write("The 3rd child is "),write(RdTitle),write(", "),write(RdName),write('.').
 
-% grandchildren
-
-% Quiz
+% TODO
